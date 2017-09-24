8293dffdf131b311447cfff277fcf9d02d3a2335
Diego Ubirajara <diego.ubirajara@mail.com>
2017-09-24 14:36:44 +0200

    update settings

--- mktplace/settings.py
+++ mktplace/settings.py
@@ -123,11 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticsfiles')
 
 SUIT_CONFIG = {
     'SHOW_REQUIRED_ASTERISK': True,
     'MENU_EXCLUDE': ('auth.group', 'auth'),
     }
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media/')
