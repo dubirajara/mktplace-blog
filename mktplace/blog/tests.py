4faaa179e8ac521c2c6654e746f78cf7fd315231
Diego Ubirajara <diego.ubirajara@mail.com>
2017-09-25 19:05:39 +0200

    add test blog view

--- mktplace/blog/tests.py
+++ mktplace/blog/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.shortcuts import resolve_url as r
 
-# Create your tests here.
+
+class BlogTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get(r('post_list'))
+
+    def test_get(self):
+        """GET 'blog' must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """'blog' must use template blog.html and base.html"""
+        self.assertTemplateUsed(self.response, 'blog.html')
+        self.assertTemplateUsed(self.response, 'base.html')
