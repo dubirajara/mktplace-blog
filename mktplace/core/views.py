aad4fb144ff694b8e63c0bde67db7ebceb3103de
Diego Ubirajara <diego.ubirajara@mail.com>
2017-10-05 19:39:47 +0200

    update

--- mktplace/core/views.py
+++ mktplace/core/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, TemplateView
+from django.conf import settings
 
 from mktplace.blog.models import Post
 
@@ -8,6 +9,13 @@ class HomeView(ListView):
     queryset = Post.objects.all()
     context_object_name = 'posts'
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['META_DESCRIPTION'] = settings.META_DESCRIPTION
+        context['META_TITLE'] = settings.META_TITLE
+
+        return context
 
 
 class ThanksContactView(TemplateView):
