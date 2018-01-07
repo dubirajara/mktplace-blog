302a444d68801701cf72ba869363e14ba2771dbf
Diego Ubirajara <diego.ubirajara@mail.com>
2018-01-07 19:32:37 +0100

    refactor test

---
+++ mktplace/blog/tests/tests_details.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from django.test.client import Client
+from django.shortcuts import resolve_url as r
+from django.contrib.auth.models import User
+
+from mktplace.blog.models import Post
+
+
+class DetailsTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.username = 'diego'
+        self.email = 'test@djangoapp.com'
+        self.password = 'test'
+        user = User.objects.create_user(
+            self.username, self.email, self.password
+        )
+
+        self.blog = Post.objects.create(
+            user=user, title='test app', tags='django'
+        )
+        self.response = self.client.get(r(self.blog.get_absolute_url()))
+
+    def test_get(self):
+        """GET 'Ideas Details' must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
+        self.assertTrue('posts' in self.response.context)
+
+    def test_get_tags(self):
+        """GET 'Ideas tags' must return status code 200"""
+        response = self.client.get(r('tag', self.blog.tags))
+        self.assertEqual(200, response.status_code)
+        self.assertTrue('tags' in response.context)
