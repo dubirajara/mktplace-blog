5f74346839c10ec8ab553c8c2e8ab9da65199cd8
Diego Ubirajara <diego.ubirajara@mail.com>
2018-01-14 12:38:23 +0100

    add test media root url

---
+++ mktplace/blog/tests/test_media.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.test import TestCase
+
+from mktplace.urls import urlpatterns
+
+
+class MediaTestUrl(TestCase):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    def setUp(self):
+        settings.DEBUG = True
+
+    def test_debug(self):
+        assert settings.DEBUG
+
+    def test_media(self):
+        assert urlpatterns
