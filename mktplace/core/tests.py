475383baf1d975435bcc4b0a9a88bdc73c4302c0
Diego Ubirajara <diego.ubirajara@mail.com>
2018-01-25 18:47:51 +0100

    add test links home

--- mktplace/core/tests.py
+++ mktplace/core/tests.py
@@ -1,9 +1,23 @@
+from django.contrib.auth.models import User
 from django.shortcuts import resolve_url as r
-from django.test import TestCase
+from django.test import TestCase, Client
+
+from mktplace.blog.models import Post
 
 
 class HomeTest(TestCase):
     def setUp(self):
+        self.client = Client()
+        self.username = 'diego'
+        self.email = 'test@djangoapp.com'
+        self.password = 'test'
+        user = User.objects.create_user(
+            self.username, self.email, self.password
+        )
+
+        self.blog = Post.objects.create(
+            user=user, title='test app', tags='django'
+        )
         self.response = self.client.get(r('home'))
 
     def test_get(self):
@@ -14,3 +28,18 @@ class HomeTest(TestCase):
         """'home' must use template index.html and base.html"""
         self.assertTemplateUsed(self.response, 'index.html')
         self.assertTemplateUsed(self.response, 'base.html')
+
+    def test_home_links(self):
+        """check href link in home"""
+        contents = (
+            'href="{}"'.format(r('post_list')),
+            'href="{}"'.format(r('post_details', self.blog.slug)),
+            'href="{}"'.format('mailto:info@themarketingplace.es'),
+            'href="{}"'.format('https://www.facebook.com/themarketingplace/'),
+            'href="{}"'.format('https://twitter.com/TMarketingPlace'),
+            'href="{}"'.format('https://www.linkedin.com/company/the-marketing-place/'),
+            'href="{}"'.format('http://themarketingplace.es/'),
+        )
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(self.response, expected)
