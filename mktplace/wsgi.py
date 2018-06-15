6289dbd8df1290fc375ce15ec411a4c02b7acc87
Diego Ubirajara <diego.ubirajara@mail.com>
2018-06-15 16:50:35 +0200

    add whitenoise to server staticfiles

--- mktplace/wsgi.py
+++ mktplace/wsgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 
-from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mktplace.settings")
 
-application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(get_wsgi_application())
